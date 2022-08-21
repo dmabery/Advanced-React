5c7a86f0b3b5978ec96f571bde55c4efe6a13921
Dalton Mabery <61725749+dmabery@users.noreply.github.com>
2022-08-21 16:15:46 -0700

    Create a form for users to add a product and handle on submit functions

--- sick-fits/frontend/components/createProduct.js
+++ sick-fits/frontend/components/createProduct.js
@@ -1,35 +1,63 @@
 import useForm from '../lib/useForm'
+import Form from './styles/Form'
 
 export default function CreateProduct() {
-    const { inputs, handleChange } = useForm({
+    const { inputs, handleChange, clearForm, resetForm } = useForm({
+        image: '',
         name: 'Nice Shoes',
         price: 5562,
         description: 'Good shoes mate.'
     });
     return (
-        <form>
-            <label htmlFor="name">
-                Name
-                <input
-                    type="text"
-                    id="name"
-                    name="name"
-                    placeholder="Name"
-                    value={inputs.name} 
-                    onChange={handleChange}
-                />
-            </label>
-            <label htmlFor="price">
-                Name
-                <input
-                    type="number"
-                    id="Price"
-                    name="price"
-                    placeholder="price"
-                    value={inputs.price} 
-                    onChange={handleChange}
-                />
-            </label>
-        </form>
+        <Form onSubmit={(e) => {
+            e.preventDefault();
+            console.log(inputs);
+        }}>
+            <fieldset>
+                <label htmlFor="image">
+                        Image
+                        <input
+                            required
+                            type="file"
+                            id="image"
+                            name="image"
+                            onChange={handleChange}
+                        />
+                    </label>
+                <label htmlFor="name">
+                    Name
+                    <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        placeholder="Name"
+                        value={inputs.name} 
+                        onChange={handleChange}
+                    />
+                </label>
+                <label htmlFor="price">
+                    Name
+                    <input
+                        type="number"
+                        id="Price"
+                        name="price"
+                        placeholder="price"
+                        value={inputs.price} 
+                        onChange={handleChange}
+                    />
+                </label>
+                <label htmlFor="description">
+                    Description
+                    <textarea
+                        id="description"
+                        name="description"
+                        placeholder="Description"
+                        value={inputs.description} 
+                        onChange={handleChange}
+                    ></textarea>
+                </label>
+                <button type="submit">+ Add Product</button>
+            </fieldset>
+        </Form>
     )
 }
