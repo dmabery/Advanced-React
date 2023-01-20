b1c1fe7f46df73c99a7726e42afbe27e3da6647f
Dalton Mabery <61725749+dmabery@users.noreply.github.com>
2023-01-19 16:03:55 -0800

    fix query param thing

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,36 +1,32 @@
-import NProgress from "nprogress";
-import Router from "next/router";
-import Page from "../components/Page";
-
+import { ApolloProvider } from '@apollo/client';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import Page from '../components/Page';
 import '../components/styles/nprogress.css';
-import { ApolloProvider } from "@apollo/client";
-import withData from "../lib/withData";
-import { Component } from "react";
+import withData from '../lib/withData';
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps, apollo }) {
-    console.log(apollo)
-    return (
-        <ApolloProvider client={apollo} >
-            <Page>
-                <Component {...pageProps} />
-            </Page>
-        </ApolloProvider>
-    );
+  console.log(apollo);
+  return (
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
+  );
 }
 
-// if any of the pages have a getInitialProps, fetch that and wait to serve the page
-
-MyApp.getInitalProps = async function({ Component, ctx }) {
-    let pageProps = {};
-    if(Component.getInitalProps) {
-        pageProps = await Component.getInitalProps(ctx);
-    }
-    pageProps.query = ctx.query;
-    return { pageProps };
-}
+MyApp.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return { pageProps };
+};
 
 export default withData(MyApp);
